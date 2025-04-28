595800426b8c94fb4fb800a2e557116fbed115c8
nbcchen <hanjun.chen@nbcuni.com>
2025-04-27 22:54:59 -0400

    delete

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    const guestName = new URL(location.href).searchParams.get('guest');
-    if (guestName) {
-        document.getElementById('guest-name').innerText = 'Hi, ' + guestName;
-    }
-};
